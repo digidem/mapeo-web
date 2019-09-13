7cc36494c2e0968ce10041563ade5a84cb341ce3
noffle <sww@eight45.net>
2019-09-13 14:19:00 -0700

    project page for a new project

--- routes/project.js
+++ routes/project.js
@@ -1,16 +1,54 @@
+var path = require('path')
+var fs = require('fs')
+
 module.exports = {
   overview
 }
 
+// TODO: use nanohtml for html escaping of user input
+
 function overview (req, res, q, params, splats) {
   var pid = params.project_id
+  var html
 
-  var html = `
-    <title>mapeo-web</title>
-    <body>
-      <h1>project overview</h1>
-      <blockquote><i>${pid}</i></blockquote>
-    </body>
+  fs.stat(path.join('projects', pid), function (err, stats) {
+    if (err && err.code === 'ENOENT') {
+      done(null, renderNewProject(pid))
+    } else if (err) {
+      done(null, renderError(err))
+    } else {
+      renderProject(pid, done)
+    }
+  })
+
+  function done (err, html) {
+    var body = `
+      <title>mapeo-web</title>
+      <body>
+        <h1>project overview</h1>
+        <blockquote><i>${pid}</i></blockquote>
+        ${html}
+      </body>
+    `
+    res.end(body)
+  }
+}
+
+function renderProject (pid, cb) {
+}
+
+function renderError (err) {
+  return `<font color=red>ERROR: ${err.toString()}</font>`
+}
+
+function renderNewProject (pid) {
+  return ` 
+    <p>This project is not currently hosted here. Would you like to have mapeo-web start seeding this project and its data?</p>
+    <p>
+      <i>Warning: this will enable any internet-capable computer with knowledge of this project ID to download & upload data to this project via this service.</i>
+    </p>
+    <form>
+      <input type="submit" value="seed"></input>
+    </form>
   `
-  res.end(html)
 }

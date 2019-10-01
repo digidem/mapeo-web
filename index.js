1ac3dd55c4d398ffeee9407d1414bfed01da07a0
noffle <sww@eight45.net>
2019-10-01 15:29:51 -0700

    refactor: load all projects on init

--- index.js
+++ index.js
@@ -18,27 +18,52 @@ var utils = {
   removeProject
 }
 
-var router = routes()
-router.addRoute('GET /', require('./routes/main'))
-router.addRoute('GET /project/:project_id', require('./routes/project_overview'))
-router.addRoute('GET /project/:project_id/filters/:filter_id/export.geojson', require('./routes/project_export'))
+console.log('Loading projects..')
+loadProjects(function (err) {
+  if (err) throw err
+  console.log('done!')
+  console.log('Starting server..')
+  startServer()
+})
 
-http.createServer(function (req, res) {
-  var parsed = url.parse(req.url)
-  var q = querystring.parse(parsed.query)
-  var str = req.method + ' ' + parsed.pathname
-  var m = router.match(str)
-  console.log(str)
-  if (m) {
-    m.fn.apply(null, [req, res, q, m.params, m.splats, utils])
-  } else {
-    res.statusCode = 404
-    res.end('no such route')
-  }
-})
-.listen(5000, function () {
-  console.log('listening on', this.address().address, 'port', this.address().port)
-})
+function startServer () {
+  var router = routes()
+  router.addRoute('GET /', require('./routes/main'))
+  router.addRoute('GET /project/:project_id', require('./routes/project_overview'))
+  router.addRoute('GET /project/:project_id/filters/:filter_id/export.geojson', require('./routes/project_export'))
+
+  http.createServer(function (req, res) {
+    var parsed = url.parse(req.url)
+    var q = querystring.parse(parsed.query)
+    var str = req.method + ' ' + parsed.pathname
+    var m = router.match(str)
+    console.log(str)
+    if (m) {
+      m.fn.apply(null, [req, res, q, m.params, m.splats, utils])
+    } else {
+      res.statusCode = 404
+      res.end('no such route')
+    }
+  })
+  .listen(5000, function () {
+    console.log('listening on', this.address().address, 'port', this.address().port)
+  })
+}
+
+function loadProjects (cb) {
+  fs.readdir('projects', function (err, files) {
+    if (err) return cb(err)
+    var pending = files.length + 1
+    files.forEach(function (file) {
+      var core = getOrCreateProject(file)
+      core.osm.ready(function () {
+        console.log('Loaded', file)
+        if (!--pending) cb()
+      })
+    })
+    if (!--pending) cb()
+  })
+}
 
 function getOrCreateProject (pid) {
   if (!projectCores[pid]) {

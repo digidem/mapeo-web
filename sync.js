dfee6b84d4190d90f686b124fba7f005b2f4d39f
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-09-30 12:32:14 -0700

    feat: connect over tcp socket

--- sync.js
+++ sync.js
@@ -1,19 +1,33 @@
 var Mapeo = require('@mapeo/core')
+var net = require('net')
 var Osm = require('osm-p2p')
 var Blob = require('safe-fs-blob-store')
+var path = require('path')
+var mkdirp = require('mkdirp')
 
-var pid = argv._[0]
-var port = arv._[1]
+var pid = process.argv._[0]
+var port = process.argv._[1]
 
 mkdirp.sync(path.join('projects', pid))
 var dbdir = path.join('projects', pid, 'db')
+
 var osm = Osm({ dir: dbdir, encryptionKey: pid })
 var media = Blob(path.join('projects', pid, 'media'))
+var mapeo = new Mapeo(osm, media)
 
-projectCores[pid] = new Mapeo(osm, media, { internetDiscovery: true })
-projectCores[pid].sync.setName('mapeo-web') // TODO: some way for the operator to provide this
-projectCores[pid].sync.listen()
-projectCores[pid].sync.join(pid)
+// TODO: some way for the operator to provide this
+mapeo.sync.setName('mapeo-web')
 
-return projectCores[pid]
+mapeo.on('peer', (peer) => {
+  mapeo.syncNetwork(peer)
+})
+
+var server = net.createServer((c) => {
+  mapeo.sync.onConnection(c, {
+  })
+})
+
+server.listen(port, () => {
+  process.send('ready')
+})
 
